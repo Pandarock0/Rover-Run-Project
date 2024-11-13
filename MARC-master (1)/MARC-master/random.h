87f892bb3b56e094669fc9a23662fd37626d9e57
LeoCrge <151128049+LeoCrge@users.noreply.github.com>
2024-11-13 10:41:42 +0100

    Add files via upload

---
+++ MARC-master (1)/MARC-master/random.h
@@ -0,0 +1,25 @@
+//
+// Created by Carouge on 01/11/2024.
+//
+
+
+#ifndef MARC_MASTER__1__RANDOM_H
+#define MARC_MASTER__1__RANDOM_H
+
+typedef enum {
+    For_10,
+    For_20,
+    For_30,
+    Back_10,
+    Turn_LEFT,
+    Turn_RIGHT,
+    U_Turn,
+} tmove;
+
+void resetCount();
+t_move selectRandomMove();
+void moveexecution ();
+
+
+
+#endif //MARC_MASTER__1__RANDOM_H

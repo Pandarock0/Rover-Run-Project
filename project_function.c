9416eec5ce379ad61fc9c176d6e7371aa1a425bc
DarfilalAbdou <151127458+DarfilalAbdou@users.noreply.github.com>
2024-11-13 14:32:08 +0100

    Start to find minimal path
    Starting to make the minimal route function.

--- project_function.c
+++ project_function.c
@@ -21,3 +21,41 @@ typedef struct node {
 
 } node;
 
+
+
+//pseudo code while waiting for tree function
+
+node** minimal_route(tree){// This function will test each combination of routes using the 5 moves then keep the lowest wieghed one
+
+    node** all_route[126]; // 9 among 5 is 126 each element is a route from root to leaf
+    for (int a=0; a<9;a++)
+    {
+        all_route[a]=create_tree(tree->*children[a])
+        for(int b=0; b<8;b++)
+        {
+            all_route[a] = add_leaf(all_route[a],
+            for(int c=0; c<7; c++)
+            {
+                for(int c=0; c<6;c++)
+                {
+                    for(int d=0; d<5;d++)
+                    {
+                        for(int e=0;e<4;e++)
+                        {
+                            for(int f=0; f=3;f++)
+                            {
+                                for(int g=0; g<2;g++)
+                                {
+                                    for(int h=0;h<1;h++)
+                                    {
+
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}

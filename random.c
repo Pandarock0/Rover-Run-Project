36f79646df370d40c77c3501ca1503de8f7f4135
DarfilalAbdou <151127458+DarfilalAbdou@users.noreply.github.com>
2024-11-13 13:33:41 +0100

    adding random for Leo

---
+++ random.c
@@ -0,0 +1,5 @@
+//
+// Created by abdel on 13/11/2024.
+//
+
+#include "random.h"
---
+++ random.h
@@ -0,0 +1,8 @@
+//
+// Created by abdel on 13/11/2024.
+//
+
+#ifndef ROVER_RUN_PROJECT_RANDOM_H
+#define ROVER_RUN_PROJECT_RANDOM_H
+
+#endif //ROVER_RUN_PROJECT_RANDOM_H

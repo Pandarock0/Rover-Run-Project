8401ffc9d226cddf14e7189069d09e71a77ea311
LeoCrge <leo.carouge7@gmail.com>
2024-11-21 15:14:36 +0100

    idk

--- project_function.h
+++ project_function.h
@@ -2,7 +2,7 @@
 // Created by brian on 13/11/2024.
 //
 
-#ifndef UNTITLED1_PROJECT_FUNCTION_H
+/*#ifndef UNTITLED1_PROJECT_FUNCTION_H
 #define UNTITLED1_PROJECT_FUNCTION_H
 
 node** minimal_route(tree);
@@ -13,3 +13,4 @@ node** minimal_route(tree);
 
 
 #endif //UNTITLED1_PROJECT_FUNCTION_H
+*/

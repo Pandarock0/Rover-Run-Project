fc04185db2528501c3140dc0fa6df04de300268a
Pandarock0 <151137645+Pandarock0@users.noreply.github.com>
2024-11-24 17:43:24 +0100

    FINAL

--- main.c
+++ main.c
@@ -2,74 +2,80 @@
 #include <time.h>
 #include <stdlib.h>
 #include "project_function.h"
-#include "map.h"
-
-
-
-void menu(){
-    printf("1. Choose the map of your choice for the rover\n");
-    printf("2. Execute a phase of 9 random moves\n");
-    printf("3. Display nodes of the tree (you need to stop the program)\n");
-    printf("4. Display nodes located at the base\n");
-    printf("5. Display MARC displacement (test)\n");
-    printf("Else: Exit\n");
-    int choice, cost;
-    t_map map = createTrainingMap();
-    t_tree tree;
-    int* moves;
-    srand(time(NULL));
-    int *list_mvmt = moveexecution();
-    Route route;
-    scanf("%d", &choice);
-    switch (choice) {
-        case 1:
-            choose_map();
-            menu();
-            break;
-
-        case 2:
-            printf("\n[");
-            moves = moveexecution();
-            for (int i = 0; i < 9; i++) {
-                printf("%d", moves[i]);
-            }
-            printf("]\n");
-            menu();
-            break;
-
-        case 3:
-            tree = create_tree(moveexecution(), map);
-            display_tree(tree.root_node);
-            menu();
-            break;
-
-
-        case 4:
-            tree = create_tree(moveexecution(), map);
-            print_base_station_nodes(tree.root_node);
-            menu();
-            break;
-        case 5:
-            displayMap_robot(map, test_function(), 3);
-            menu();
-            break;
-
-        case 6:
-            // how can we display the best route?
-            tree = create_tree(list_mvmt, map);
-            route = minimum_route(tree);
-            printf("The best route is %d", route.length);
-            menu();
-            break;
-        default:
-            break;
-
-    }
-}
 
 
 
 int main() {
-    menu();
+    t_map map = createMapFromFile("..\\maps\\example1.map");
+    printf("Map created with dimensions %d x %d\n", map.y_max, map.x_max);
+    for (int i = 0; i < map.y_max; i++)
+    {
+        for (int j = 0; j < map.x_max; j++)
+        {
+            printf("%d ", map.soils[i][j]);
+        }
+        printf("\n");
+    }
+    // printf the costs, aligned left 5 digits
+    for (int i = 0; i < map.y_max; i++)
+    {
+        for (int j = 0; j < map.x_max; j++)
+        {
+            printf("%-5d ", map.costs[i][j]);
+        }
+        printf("\n");
+    }
+    displayMap(map);
+
+
+
+    srand(time(NULL));
+    int *list_mvmt = moveexecution();
+    //map = createTrainingMap();
+
+    t_tree tree = create_tree(list_mvmt, map);
+
+    t_node** best_route_node = (t_node**)malloc(sizeof(t_node*));
+    int size_temp = 0;
+    t_node** best_route = Best_route_node(tree.root_node, &size_temp, NULL, best_route_node);
+    int node_count = best_route[0]->depth;
+
+
+
+    while (1) {
+
+        int val;
+        printf("Testing function :");
+        printf("\n1.List of random movement\n2.Display some nodes of the tree (You need to stop the program)\n3.Display nodes located at the base\n4.Display all the movement to go to the base\n5.Display MARC displacement (test)\n6.Exit\n");
+        scanf("%d", &val);
+        if (val == 1){
+            printf("\n[");
+            for(int i=0; i<9; i++){
+                printf(" %d ", list_mvmt[i]);
+            }
+            printf("]\n");
+        }
+        else if (val == 2){
+            display_tree(tree.root_node);
+        }
+        else if (val == 3){
+            print_base_station_nodes(tree.root_node);
+        }
+        else if (val == 4){
+
+            t_node** best_route_node_list =  Best_route_node_list(best_route);
+            display_best_move(best_route_node_list, node_count);
+
+        }
+        else if (val == 5) {
+            displayMap_robot(map, test_function(), node_count);
+        }
+        else if (val == 6 || val != 1 || val != 2 || val != 3 || val !=4 || val != 5){
+            break;
+        }
+    }
+
     return 0;
 }
+
+//comment test
